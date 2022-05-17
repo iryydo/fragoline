4c2b4c5e1a56154dd39dca768b7839da573d3e8e
Irina Dochia <iryydo@gmail.com>
2022-05-17 21:59:56 +0200

    settato swiper e aggiunto grid su desktop view

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,7 +1,15 @@
 const swiper = new Swiper('.swiper', {
 
-    loop: true,
-
+    enabled: true,
+    speed: 400,
+    initialSlide: 0,
+    spaceBetween: 0,
+    effect: 'slide',
+    direction: 'horizontal',
+    slidesPerView: 1,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    grabCursor: true,
     scrollbar: {
         el: '.swiper-scrollbar',
     },
