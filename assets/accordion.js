bdd573645c6a5bb22165a206177762915aa7e7a8
Irina Dochia <iryydo@gmail.com>
2022-05-17 19:36:24 +0200

    aggiunto toggle con il segno e modificato altezza svg e script toggle

--- assets/accordion.js
+++ assets/accordion.js
@@ -3,5 +3,9 @@ accordionTitle.forEach((e) =>
     e.addEventListener('click', toggleAccordion)
 );
 function toggleAccordion(event) {
+    console.log(event)
     event.target.nextElementSibling.classList.toggle('open');
+
+    event.target.classList.toggle('minus-icon')
 }
+
