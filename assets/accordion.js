efc878011d2ae5fb3b77878cd9391fcada1bbb9f
Irina Dochia <iryydo@gmail.com>
2022-05-17 22:24:54 +0200

    creazione accordion

--- assets/accordion.js
+++ assets/accordion.js
@@ -3,9 +3,19 @@ accordionTitle.forEach((e) =>
     e.addEventListener('click', toggleAccordion)
 );
 function toggleAccordion(event) {
-    console.log(event)
     event.target.nextElementSibling.classList.toggle('open');
 
-    event.target.classList.toggle('minus-icon')
+    event.target.classList.toggle('minus-icon');
+
+    const paragraph = this.nextElementSibling;
+
+    console.log(paragraph);
+
+
+    if (paragraph.style.display === "block") {
+        paragraph.style.display = "none";
+    } else {
+        paragraph.style.display = "block";
+    }
 }
 
