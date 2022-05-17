622bb0e0264e6bd40b9d1d1a437ba63a1002e293
Irina Dochia <iryydo@gmail.com>
2022-05-17 11:17:55 +0200

    creato script chiusura banner nero su tutte le pagine

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,14 @@
+/*  */
+
+/* CLOSE BANNER */
+const blackBannerButton = document.querySelector("#close-banner");
+
+blackBannerButton.addEventListener("click", closeBanner);
+
+function closeBanner(event) {
+    this.parentNode.style.display = "none";
+}
+
 /* CAROSELLO */
 
 const swiper = new Swiper('.swiper', {
